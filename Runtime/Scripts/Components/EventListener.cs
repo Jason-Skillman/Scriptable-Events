c3363aecbe58f28d43911eb387d750efb32f7d98
Jason Skillman <jason_skillman@outlook.com>
2023-03-06 21:30:46 -0500

    Fix null bug and create listener script

---
+++ Runtime/Scripts/Components/EventListener.cs
@@ -0,0 +1,25 @@
+﻿namespace JasonSkillman.ScriptableEvents.Components {
+	using UnityEngine;
+	using UnityEngine.Events;
+
+	public class EventListener : MonoBehaviour {
+
+		[SerializeField]
+		private VoidEvent @event;
+		[SerializeField]
+		private UnityEvent onInvoke;
+
+		private void OnEnable() {
+			if(@event == null) return;
+			@event.OnInvoked -= OnInvoked;
+			@event.OnInvoked += OnInvoked;
+		}
+
+		private void OnDisable() {
+			if(@event == null) return;
+			@event.OnInvoked -= OnInvoked;
+		}
+
+		private void OnInvoked() => onInvoke?.Invoke();
+	}
+}
